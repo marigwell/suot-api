1f2fcf1b87edd3bd2b737d84fe9b1b10941c257d
Jim Marigmen <jwellmarigmen@gmail.com>
2026-07-29 19:37:16 -0500

    add alembic migrations

--- app/main.py
+++ app/main.py
@@ -1,12 +1,12 @@
 from fastapi import FastAPI
 
-from app.database import Base, engine
+# from app.database import Base, engine
 from app.models import item  # noqa: F401
 from app.routers import health, items
 
-Base.metadata.create_all(bind=engine)
+# Base.metadata.create_all(bind=engine)
 
-app = FastAPI()
+app = FastAPI(title="Suot API")
 
 app.include_router(health.router)
 app.include_router(items.router)
--- app/models/item.py
+++ app/models/item.py
@@ -8,7 +8,7 @@ from app.database import Base
 class ItemModel(Base):
     __tablename__ = "items"
 
-    id: Mapped[int] = mapped_column(primary_key=True, index=True)
+    id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column(String(100), nullable=False)
     category: Mapped[str] = mapped_column(String(100), nullable=False)
     color: Mapped[str] = mapped_column(String(100), nullable=False)
